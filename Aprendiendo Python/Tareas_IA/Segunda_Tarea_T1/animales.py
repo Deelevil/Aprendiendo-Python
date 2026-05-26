a900f4a8ed0e83867384ddbae08f4c0686531f68
Deelevil <xian.eiros@gmail.com>
2026-05-26 18:59:44 +0200

    Terminada la tarea 2

--- Aprendiendo Python/Tareas_IA/Segunda_Tarea_T1/animales.py
+++ Aprendiendo Python/Tareas_IA/Segunda_Tarea_T1/animales.py
@@ -10,26 +10,48 @@ except FileNotFoundError :
     print("No se encontro datos guardados. Se crearan al terminar")
 
 while True :
-    decision = input("Desea introducir un nuevo animal? (s/n)")
+    decision = input("Desea introducir un nuevo animal? (s/n):")
     if decision.lower() == 'n':
         break
     try:
-        n = input("Cuantos animales quires introducir?")
+        n = int(input("Cuantos animales quires introducir?:"))
         info_refugio = {}
-        #Tenemos a variable de repetición e o diccionario de cada animal
+        
+            #Tenemos a variable de repetición e o diccionario de cada animal
         for i in range(n):
-            nombre = input("Nº del Chip {}".format(i+1))
-            especie = input("Nombre de la especie {}".format(i+1))
-            caracteristicas_n = input("Total de caracteristicas del animal {}".format(i+1))
-    
+            try:
+                nombre = input("Nº del Chip {}:".format(i+1))
+                especie = input("Nombre de la especie {} :".format(i+1))
+                caracteristicas_n = int(input("Total de caracteristicas del animal {}".format(i+1)))
+                caracteristicas_b = {}
+                for j in range(caracteristicas_n):
+                    concepto = input("Que tipo de caracteristica es?(ej:Planeta,Peligroso,etc):")
+                    cualidad = input(f"Ingrese la caracteristica ")
+                    #Ahora guardamos esta mini cajita jejejeje
+                    caracteristicas_b[concepto] =  cualidad
+
+                refugio.append({
+                "Chip id" : nombre,
+                "Especie" : especie,
+                "caracteristicas" : caracteristicas_b
+                }) 
+            except ValueError :
+                print("Ocurrio un error de nº. Revisa bien los formatos que introduzcas")
 
 
 
-    except ValueError:
-        print("Fijate en los formatos de datos que estas añadiendo")
+   
     except Exception as error:
         print("Ocurrio un error inesperado", str(error))
 
+exportar = input("Desea exportar los datos a un formato JSON?  (s/n):")
+if exportar.lower() == 's' :
+        with open("refugio.json" , "w") as f:
+            json.dump (refugio , f , indent=4)
+            print("Datos exportados correctamente")
+else:
+    print("Exportación cancelada")
+    
+
+
 #Linea Final
-
-
